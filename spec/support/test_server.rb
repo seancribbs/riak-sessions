47a65bcb2e35873375ba42c79ed02e85b3a84809
Sean Cribbs <sean@basho.com>
2012-02-03 11:32:15 -0500

    Stop the test server at the end of the suite instead of in an at_exit block.

--- spec/support/test_server.rb
+++ spec/support/test_server.rb
@@ -1,15 +1,13 @@
-
 require 'riak/test_server'
 
 RSpec.configure do |config|
   config.before do
     unless $test_server
       begin
-        config = YAML.load_file("spec/support/test_server.yml")
+        config = YAML.load_file(File.expand_path("../test_server.yml", __FILE__))
         $test_server = Riak::TestServer.create(:root => config['root'],
                                                :source => config['source'],
                                                :min_port => config['min_port'] || 15000)
-        at_exit { $test_server.stop }
       rescue => e
         $stderr.puts "Can't run riak-sessions specs without the test server. Specify the location of your Riak installation in spec/support/test_server.yml"
         $stderr.puts e.inspect
@@ -18,7 +16,12 @@ RSpec.configure do |config|
     end
     $test_server.start
   end
+
   config.after do
     $test_server.drop
+  end
+
+  config.after(:suite) do
+    $test_server.stop
   end
 end
